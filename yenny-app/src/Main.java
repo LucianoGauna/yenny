aa15ce5b871a1fda24e9a656e972020718d0069e
Luciano Gauna <luciano.927@hotmail.com>
2025-09-18 15:55:24 -0300

    transformar id de Usuario a int (al menos de momento)

--- yenny-app/src/Main.java
+++ yenny-app/src/Main.java
@@ -3,8 +3,8 @@ import domain.Usuario;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario admin  = new Usuario(1L, "admin",  "admin123",  Rol.ADMIN);
-        Usuario cajero = new Usuario(2L, "cajero", "cajero123", Rol.CAJERO);
+        Usuario admin  = new Usuario(1, "admin",  "admin123",  Rol.ADMIN);
+        Usuario cajero = new Usuario(2, "cajero", "cajero123", Rol.CAJERO);
         System.out.println(admin);
         System.out.println(cajero);
     }
--- yenny-app/src/domain/Usuario.java
+++ yenny-app/src/domain/Usuario.java
@@ -1,21 +1,21 @@
 package domain;
 
 public class Usuario {
-    private Long id;
+    private int id;
     private String username;
     private String password;
     private Rol rol;
     private boolean activo = true;
 
-    public Usuario(Long id, String username, String password, Rol rol) {
+    public Usuario(int id, String username, String password, Rol rol) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.rol = rol;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
